2c44a27f8347b80d63f3d45c5b3441f62a6e109c
Alejandroguerra4915 <alejandro.guerrab@upb.edu.co>
2023-02-23 17:23:13 -0500

    commi 4

--- Assets/Future dog z/Scripts/controlmenu.cs
+++ Assets/Future dog z/Scripts/controlmenu.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
+using UnityEngine.SceneManagement;
 
 public class controlmenu : MonoBehaviour
 {
-  public void OnpressJuegar()
+  public void OnpressJugar()
     {
         MMSceneLoadingManager.LoadScene("nivel1");
+        SceneManager.LoadScene("nivel1", LoadSceneMode.Single);
     }
 
     public void Onpressinstrucciones()
     {
         MMSceneLoadingManager.LoadScene("instrucciones");
+        SceneManager.LoadScene("instrucciones", LoadSceneMode.Single);
+    }
+
+    public void OnpressVolverMenu()
+    {
+        MMSceneLoadingManager.LoadScene("menu");
+        SceneManager.LoadScene("menu", LoadSceneMode.Single);
     }
 
      public void OnpressSalir()
